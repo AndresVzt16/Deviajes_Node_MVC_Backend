9102f640be9cfb0dbcf83edaff8a2d7a9d304c14
Andres Vizuete <andresv0807@gmail.com>
2024-07-01 18:51:00 -0500

    Adicion de rutas de password

--- backend/routes/usuarioRoutes.js
+++ backend/routes/usuarioRoutes.js
@@ -8,6 +8,10 @@ const router = Router();
 router.post('/login', autenticarUsuario)
 router.post('/', registrarUsuario)
 
+router.post('/olvide-password', generarCambio)
+
+router.route('/olvide-password/:token')
+    .get(validarToken)
 
 //Private
 router.post('/perfil',obtenerPerfil)
